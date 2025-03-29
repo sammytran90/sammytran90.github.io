bc729ee17939548a9035884033705d60f2dfeedc
trananh-tt-intervideo <32162425+sammytran90@users.noreply.github.com>
2025-03-29 22:38:38 +0700

    update profile

--- code/routes/index.tsx
+++ code/routes/index.tsx
@@ -1,40 +1,144 @@
-
 export default function Home() {
   return (
     <div class="px-4 py-8 mx-auto bg-white">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <h1 class="text-4xl font-bold my-6">Anh Trung Tran</h1>
+        <p class="text-xl text-gray-600 mb-2">I create software everyday</p>
+        
         <img
           class="my-6 rounded-full"
           src="/profile.jpg"
-          width="128"
-          height="128"
+          width="400"
+          height="300"
           alt="Profile Picture"
         />
-        <h2 class="text-2xl font-semibold my-4">About Me</h2>
+        <h2 class="text-2xl font-semibold my-4">Summary</h2>
         <p class="my-4 text-center">
-          I am a software developer with a passion for creating web applications. I have experience in various technologies including JavaScript, TypeScript, and React.
+          A software engineer with enthusiasm to explore innovative ideals, and thrilled to use these for a better quality of life, resource management, and environmental protection
         </p>
+        
+        <h2 class="text-2xl font-semibold my-4">What I will do when I have time</h2>
+        <ul class="list-disc list-inside my-4">
+          <li>Watch podcasts</li>
+          <li>Read books</li>
+          <li>Play board games</li>
+          <li>Meditate</li>
+          <li>Casting</li>
+        </ul>
+        
         <h2 class="text-2xl font-semibold my-4">Experience</h2>
-        <ul class="list-disc list-inside my-4">
-          <li>Software Developer at XYZ Company (2020 - Present)</li>
-          <li>Frontend Developer at ABC Inc. (2018 - 2020)</li>
-        </ul>
+        <div class="w-full">
+          <div class="mb-6">
+            <h3 class="text-xl font-semibold">Freelance</h3>
+            <p class="text-lg text-gray-700">Senior Software Developer</p>
+            <p class="text-gray-600">2021 - Present (4 years)</p>
+            <ul class="list-disc list-inside mt-2">
+              <li>Develop 3D, AR/VR web applications</li>
+              <li>Integrate CMS system to manage 3D assets</li>
+              <li>Research and provide solutions for outlying challenges</li>
+              <li>Manage deployments to different platforms: Azure, Netlify, Github, Amazon Web Services</li>
+              <li>Contributions to worldwide brands</li>
+            </ul>
+          </div>
+          
+          <div class="mb-6">
+            <h3 class="text-xl font-semibold">DataScope Systems</h3>
+            <p class="text-lg text-gray-700">Software Developer</p>
+            <p class="text-gray-600">January 2022 - October 2023 (1 year 10 months)</p>
+            <p class="text-gray-500">Chester, England, United Kingdom</p>
+            <ul class="list-disc list-inside mt-2">
+              <li>Research and implement Microservices</li>
+              <li>Develop Internal Tool Web Applications</li>
+              <li>Collaborating with Helpdesk and Project Owners on issues and maintenance</li>
+            </ul>
+          </div>
+          
+          <div class="mb-6">
+            <h3 class="text-xl font-semibold">MIS Active Management Systems Limited</h3>
+            <p class="text-lg text-gray-700">Developer</p>
+            <p class="text-gray-600">July 2020 - May 2021 (11 months)</p>
+            <p class="text-gray-500">Northwich, England, United Kingdom</p>
+            <ul class="list-disc list-inside mt-2">
+              <li>Maintain a housing management solution for letting agencies in the UK</li>
+              <li>Analyse requirements and develop new features using Angular, .NET Core</li>
+              <li>Collaborate with team members to resolve outstanding issues</li>
+              <li>Agile team management</li>
+            </ul>
+          </div>
+          
+          <div class="mb-6">
+            <h3 class="text-xl font-semibold">FiinGroup JSC (previously StoxPlus)</h3>
+            <p class="text-lg text-gray-700">Software Engineer</p>
+            <p class="text-gray-600">January 2019 - August 2019 (8 months)</p>
+            <p class="text-gray-500">Hanoi, Vietnam</p>
+            <ul class="list-disc list-inside mt-2">
+              <li>Develop a financial data analysis platform for the Vietnam market</li>
+              <li>Implement chart components for large datasets with high performance</li>
+              <li>Collaborate in an Agile managed team</li>
+            </ul>
+          </div>
+          
+          <div class="mb-6">
+            <h3 class="text-xl font-semibold">Miyatsu Viet Nam</h3>
+            <p class="text-lg text-gray-700">Software Developer</p>
+            <p class="text-gray-600">August 2016 - December 2018 (2 years 5 months)</p>
+            <p class="text-gray-500">Vietnam</p>
+            <ul class="list-disc list-inside mt-2">
+              <li>Work with a range of projects consecutively</li>
+              <li>Collaborate with UI/UX designers to deliver quality products</li>
+              <li>Collaborate with QA team to ensure quality products are delivered to clients</li>
+            </ul>
+          </div>
+          
+          <div class="mb-6">
+            <h3 class="text-xl font-semibold">Oxfam</h3>
+            <p class="text-lg text-gray-700">Mobile Developer</p>
+            <p class="text-gray-600">September 2017 - January 2018 (5 months)</p>
+            <p class="text-gray-500">Hanoi, Vietnam</p>
+            <ul class="list-disc list-inside mt-2">
+              <li>Develop a mobile prototype that promotes disaster awareness in the community</li>
+              <li>Collaborate with business owner and UI/UX designer to deliver a viable solution</li>
+            </ul>
+          </div>
+        </div>
+        
         <h2 class="text-2xl font-semibold my-4">Education</h2>
+        <div class="w-full mb-6">
+          <h3 class="text-xl font-semibold">Sheffield Hallam University</h3>
+          <p class="text-lg">Master of Science - MSc, Computing (2019 - 2021)</p>
+          
+          <h3 class="text-xl font-semibold mt-4">Université Jean Moulin (Lyon III)</h3>
+          <p class="text-lg">Bachelor's degree, Marketing (2009 - 2013)</p>
+        </div>
+        
+        <h2 class="text-2xl font-semibold my-4">Top Skills</h2>
         <ul class="list-disc list-inside my-4">
-          <li>Bachelor's Degree in Computer Science from University of Somewhere (2014 - 2018)</li>
-        </ul>
-        <h2 class="text-2xl font-semibold my-4">Skills</h2>
-        <ul class="list-disc list-inside my-4">
+          <li>Cursor</li>
+          <li>Unreal Engine</li>
+          <li>Material UI</li>
           <li>JavaScript</li>
           <li>TypeScript</li>
           <li>React</li>
           <li>Node.js</li>
         </ul>
+        
+        <h2 class="text-2xl font-semibold my-4">Certifications</h2>
+        <ul class="list-disc list-inside my-4">
+          <li>Work-Life Blend: The New Work-Life Balance</li>
+          <li>Ten Habits of Great Decision Makers</li>
+        </ul>
+        
         <h2 class="text-2xl font-semibold my-4">Contact</h2>
-        <p class="my-4 text-center">
-          Email: john.doe@example.com
-        </p>
+        <div class="my-4 text-center">
+          <p>Hanoi, Vietnam</p>
+          <p>+84865639119 (Mobile)</p>
+          <p>trung.trananh0907@gmail.com</p>
+          <p>
+            <a href="https://www.linkedin.com/in/anh-trung-tran" target="_blank" class="text-blue-500 hover:underline">
+              www.linkedin.com/in/anh-trung-tran
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );

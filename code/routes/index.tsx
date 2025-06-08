b9ecb658774b65bcbe3316a72693a8f799d1161c
trananh-tt-intervideo <32162425+sammytran90@users.noreply.github.com>
2025-06-08 10:21:55 +0700

    update profile picture

--- code/routes/index.tsx
+++ code/routes/index.tsx
@@ -1,149 +1,19 @@
-
 export default function Home() {
   return (
     <div class="px-4 py-8 mx-auto bg-white">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <h1 class="text-4xl font-bold my-6">Anh Trung Tran</h1>
-        <p class="text-xl text-gray-600 mb-2">I create software everyday</p>
-        <p class="text-gray-500 mb-4">Hanoi Capital Region</p>
-        
-        <img
-          class="my-6 rounded-full"
-          src="/profile.jpg"
-          width="400"
-          height="300"
-          alt="Profile Picture"
-        />
-        <h2 class="text-2xl font-semibold my-4">About me</h2>
-        <p class="my-4 text-center">
-          I originated from a marketing background and switched to becoming a programmer when I started creating websites. My first project was a web-based Tetris game, which I created and played locally.
-        </p>
-        <p class="my-4 text-center">
-          Afterwards, I continued creating websites for 2 years before deciding to have an MSc in Computing at Sheffield Hallam University (SHU) in the UK. The decision to get back to school resulted in one of the most challenging but valuable periods ever. During this page in my life, I was exposed to a global and dynamic culture, thus forcing myself to be more organized and disciplined. I have learned to man up when Covid-19 came, as I had to manage things by myself in a foreign country.
-        </p>
-        <p class="my-4 text-center">
-          As time flew, I was fortunate enough to get out of university with a distinction and even more to secure myself with IT-related professionals in fast-paced and challenge-welcome environments. These have been precious experiences to me.
-        </p>
-        
-        <h2 class="text-2xl font-semibold my-4">My hobbies</h2>
-        <ul class="list-disc list-inside my-4">
-          <li>I play Brilliant everywhere :)</li>
-          <li>I go off-road in my car, and when I rest, I listen to music while coding</li>
-        </ul>
-        
-        <h2 class="text-2xl font-semibold my-4">Experience</h2>
+        <h1 class="text-4xl font-bold my-6 text-center">
+          Hi, glad to see you here!
+        </h1>
 
-        <div class="w-full">
-
-          <div class="mb-10">
-            <h3 class="text-xl font-semibold">Freelance</h3>
-            <p class="text-lg text-gray-700">Senior Software Developer</p>
-            <p class="text-gray-600">2021 - Present (4 years)</p>
-            <ul class="list-disc list-inside mt-2">
-              <li>Develop 3D, AR/VR web applications</li>
-              <li>Integrate CMS system to manage 3D assets</li>
-              <li>Research and provide solutions for outlying challenges, taking into account different perspectives from various stakeholders</li>
-              <li>Manage deployments to different platforms: Azure, Netlify, Github, Amazon Web Services</li>
-              <li>Contributions to worldwide brands</li>
-            </ul>
-          </div>
-          <br></br>
-          <div class="mb-10">
-            <h3 class="text-xl font-semibold">DataScope Systems</h3>
-            <p class="text-lg text-gray-700">Software Developer</p>
-            <p class="text-gray-600">January 2022 - October 2023 (1 year 10 months)</p>
-            <p class="text-gray-500">Chester, England, United Kingdom</p>
-            <ul class="list-disc list-inside mt-2">
-              <li>Research and implement Microservices</li>
-              <li>Develop Internal Tool Web Applications</li>
-              <li>Collaborating with Helpdesk and Project Owners on issues and maintenance</li>
-            </ul>
-          </div>
-          <br></br>
-          <div class="mb-10">
-            <h3 class="text-xl font-semibold">MIS Active Management Systems Limited</h3>
-            <p class="text-lg text-gray-700">Developer</p>
-            <p class="text-gray-600">July 2020 - May 2021 (11 months)</p>
-            <p class="text-gray-500">Northwich, England, United Kingdom</p>
-            <ul class="list-disc list-inside mt-2">
-              <li>Maintain a housing management solution for letting agencies in the UK</li>
-              <li>Analyse requirements and develop new features using Angular, .NET Core</li>
-              <li>Collaborate with the team leader and team members to resolve outstanding issues</li>
-              <li>Agile team management</li>
-            </ul>
-          </div>
-          <br></br>
-          <div class="mb-10">
-            <h3 class="text-xl font-semibold">FiinGroup JSC (previously StoxPlus)</h3>
-            <p class="text-lg text-gray-700">Software Engineer</p>
-            <p class="text-gray-600">January 2019 - August 2019 (8 months)</p>
-            <p class="text-gray-500">Hanoi, Vietnam</p>
-            <ul class="list-disc list-inside mt-2">
-              <li>Develop a financial data analysis platform for the Vietnam market</li>
-              <li>Implement chart components that represent large datasets with high performance</li>
-              <li>Collaborate in an Agile managed team</li>
-            </ul>
-          </div>
-          <br></br>
-          <div class="mb-10">
-            <h3 class="text-xl font-semibold">Miyatsu Viet Nam</h3>
-            <p class="text-lg text-gray-700">Software Developer</p>
-            <p class="text-gray-600">August 2016 - December 2018 (2 years 5 months)</p>
-            <p class="text-gray-500">Vietnam</p>
-            <ul class="list-disc list-inside mt-2">
-              <li>Work with a range of projects consecutively</li>
-              <li>Collaborate with UI/UX designers to deliver quality products</li>
-              <li>Collaborate with QA team to ensure quality products are delivered to the clients</li>
-            </ul>
-          </div>
-          <br></br>
-          <div class="mb-10">
-            <h3 class="text-xl font-semibold">Oxfam</h3>
-            <p class="text-lg text-gray-700">Mobile Developer</p>
-            <p class="text-gray-600">September 2017 - January 2018 (5 months)</p>
-            <p class="text-gray-500">Hanoi, Vietnam</p>
-            <ul class="list-disc list-inside mt-2">
-              <li>Develop a mobile prototype that promotes disaster awareness in the community</li>
-              <li>Collaborate with business owner and UI/UX designer to deliver a viable solution for the targeted users to provide accessible training to mitigate damages when natural disasters occur</li>
-            </ul>
-          </div>
-        </div>
-        <h2 class="text-2xl font-semibold my-4">Education</h2>
-        <div class="w-full mb-6">
-          <h3 class="text-xl font-semibold">Sheffield Hallam University</h3>
-          <p class="text-lg">Master of Science - MSc, Computing (2019 - 2021)</p>
-          
-          <h3 class="text-xl font-semibold mt-4">Université Jean Moulin (Lyon III)</h3>
-          <p class="text-lg">Bachelor's degree, Marketing (2009 - 2013)</p>
-        </div>
-        
-        <h2 class="text-2xl font-semibold my-4">Top Skills</h2>
-        <ul class="list-disc list-inside my-4">
-          <li>Cursor</li>
-          <li>Unreal Engine</li>
-          <li>Material UI</li>
-          <li>JavaScript</li>
-          <li>TypeScript</li>
-          <li>React</li>
-          <li>Node.js</li>
-        </ul>
-        
-        <h2 class="text-2xl font-semibold my-4">Certifications</h2>
-        <ul class="list-disc list-inside my-4">
-          <li>Work-Life Blend: The New Work-Life Balance</li>
-          <li>Ten Habits of Great Decision Makers</li>
-        </ul>
-        
-        <h2 class="text-2xl font-semibold my-4">Contact</h2>
-        <div class="my-4 text-center">
-          <p>Hanoi, Vietnam</p>
-          <p>+84865639119 (Mobile)</p>
-          <p>trung.trananh0907@gmail.com</p>
-          <p>
-            <a href="https://www.linkedin.com/in/anh-trung-tran" target="_blank" class="text-blue-500 hover:underline">
-              www.linkedin.com/in/anh-trung-tran
-            </a>
-          </p>
+        <div class="relative w-[200px] h-[200px] rounded-[50%] overflow-hidden">
+          <img
+            class="w-[400px] aspect-square object-contain absolute scale-[3.5]"
+            width={200}
+            height={200}
+            src="/profile.jpg"
+            alt="Profile Picture"
+          />
         </div>
       </div>
     </div>

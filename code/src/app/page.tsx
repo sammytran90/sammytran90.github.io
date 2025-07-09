e0f1e7f8e3974b7c3fc02cc437f2b2bf73ea8a5f
trananh-tt-intervideo <32162425+sammytran90@users.noreply.github.com>
2025-07-09 18:25:34 +0700

    .

--- code/src/app/page.tsx
+++ code/src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Page() {
   return (
-    <div className="flex flex-col gap-4 w-[300px] h-[300px] bg-gray-200">
-      Hello world
+    <div className="px-4 py-8 mx-auto bg-white">
+      <div className="max-w-screen-md mt-10 mx-auto flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold my-6 text-center">
+            Hi, see my works below
+        </h1>
+      </div>
     </div>
   );
 }
